afffd5b2db5d63e51c4328abc8828fede0517f31
이수진 <lsj4329@i-commtech.com>
2021-07-08 10:00:11 +0900

    제일먼저 각 부분 영역 나누기 컴포넌트 이해를 못했기 때문에 App.vue에서 한번에 작성 → 출력하면 생각했던 영역별로 출력됨

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-
+  <div id="app">
+    <div>
+      TodoList 수진
+    </div>
+    <div>
+      inputBox + add Button
+    </div>
+    <div>
+      List 출력 삭제버튼 만들기 + 상태변경 할수있게 만들기
+    </div>
+    <div>
+      전체삭제버튼 만들기
+    </div>
+  </div>
 </template>
 
 <script>
 
+
+export default {
+
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
