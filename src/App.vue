1140f10fb59cacdb115989f0cbe828fc44ecd3f5
이수진 <lsj4329@i-commtech.com>
2021-07-08 10:03:47 +0900

    제일먼저 inputBox와 추가버튼 생성하기 기능이 들어간게 아니라 template만 작성

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       TodoList 수진
     </div>
     <div>
-      inputBox + add Button
+      inputBox + add Button<br />
+      <input type="text" placeholder="할 일을 입력해주세요" >
+      <button type="button">추가</button>
     </div>
     <div>
       List 출력 삭제버튼 만들기 + 상태변경 할수있게 만들기
