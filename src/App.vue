3568575efe56943eb35feda6524f5fab9fa578f3
이수진 <lsj4329@i-commtech.com>
2021-07-08 17:05:58 +0900

    완료 목록과 못한 목록 영역별로 뜨게하기위해 영역 나누기

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
-  <div id="app">
-    <div>
-      TodoList 수진
-    </div>
-    <div>
-      inputBox + add Button<br />
-      <input type="text" v-model="todoItem" placeholder="할 일을 입력해주세요" >
-      <button @click="add">추가</button>
-    </div>
-    <div>
-      List 출력 및 삭제버튼 만들기 + 상태변경 할수있게 만들기<br />
-      <ul id="list">
-        <li v-for="(item,index) in todoItems" :key="index" >
-          <input type="checkbox"  v-model="item.complete">
-          {{ index }} | {{ item.todo }}  | {{item.complete}}
-          <button @click="del(item, index)">삭제</button>
-        </li>
-      </ul>
-    </div>
-    <div>
-      전체삭제버튼 만들기<br />
-      <button @click="allDel">전체 삭제</button>
-    </div>
+    <div id="app">
+      <div>
+        TodoList 수진
+      </div>
+      <div>
+        inputBox + add Button<br />
+        <input type="text" v-model="todoItem" placeholder="할 일을 입력해주세요" >
+        <button @click="add">추가</button>
+      </div>
+      <div>
+        List 출력 및 삭제버튼 만들기 + 상태변경 할수있게 만들기<br />
+        <ul id="list">
+          <li v-for="(item,index) in todoItems" :key="index" >
+            <input type="checkbox"  v-model="item.complete">
+            {{ index }} | {{ item.todo }}  | {{item.complete}}
+            <button @click="del(item, index)">삭제</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        전체삭제버튼 만들기<br />
+        <button @click="allDel">전체 삭제</button>
+      </div>
+      <div>
+      완료목록만 뜨게 하기
+     </div>
+      <div>
+      못한목록만 뜨게 하기
+      </div>
   </div>
 </template>
 
